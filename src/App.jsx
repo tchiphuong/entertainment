501de27f955fcce176a757f481d380ee8094794d
tcphuong <chiphuong9299@hotmail.com>
2025-11-02 20:28:55 +0700

    feat: switch to BrowserRouter for clean URLs on GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { HashRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Vods from "./pages/Vods";
 import VodPlay from "./pages/VodPlay";
 
 export default function App() {
     return (
-        <HashRouter>
+        <BrowserRouter basename="/entertainment">
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/vods" element={<Vods />} />
                 <Route path="/vods/play" element={<VodPlay />} />
             </Routes>
-        </HashRouter>
+        </BrowserRouter>
     );
 }
