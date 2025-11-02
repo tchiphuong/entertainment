56c3779953968565d639c32ad8e22b591148ebd0
tcphuong <chiphuong9299@hotmail.com>
2025-11-02 20:06:07 +0700

    fix: use HashRouter for GitHub Pages routing compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Vods from "./pages/Vods";
 import VodPlay from "./pages/VodPlay";
 
 export default function App() {
     return (
-        <BrowserRouter basename="/entertainment/">
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/vods" element={<Vods />} />
                 <Route path="/vods/play" element={<VodPlay />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
