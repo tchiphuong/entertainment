2b14b51f70d6d8746de550d55f3e35211d1df967
tcphuong <chiphuong9299@hotmail.com>
2026-02-12 01:28:50 +0700

    commit

--- src/pages/VodPlay.jsx
+++ src/pages/VodPlay.jsx
@@ -3436,7 +3436,7 @@ export default function VodPlay() {
                                     </div>
 
                                     {/* Hàng 2: Các switch settings */}
-                                    <div className="flex flex-wrap items-center justify-end gap-x-4 gap-y-2">
+                                    <div className="flex flex-wrap items-center justify-end gap-2">
                                         {/* Switch bỏ qua intro + input */}
                                         <div className="flex items-center gap-2">
                                             <div
